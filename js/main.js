d7da83962d855b8ab5be3994d0f23550d099fa6a
Kiran Anand <liteninkiran@hotmail.com>
2024-03-23 15:03:59 +0000

    15 - Activity: Your first visualization

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-d3.json('./data/ages.json').then(data => {
-    console.log(data);
-    data.forEach(d => d.age = Number(d.age));
+d3.json('./data/buildings.json').then(data => {
+    data.forEach(d => d.height = Number(d.height));
+    const border = 'border: 1px solid lightgrey; border-radius: 20px;';
+    const svg = d3.select('#chart-area')
+        .append('svg')
+        .attr('style', border)
+        .attr('width', 500)
+        .attr('height', 500);
 
-    const svg = d3.select('#chart-area').append('svg')
-        .attr('width', 400)
-        .attr('height', 400);
+    const rects = svg.selectAll('rect').data(data);
 
-    const circles = svg.selectAll('circle')
-        .data(data);
+    rects.enter()
+        .append('rect')
+        .attr('x', (d, i) => (i * 80) + 50)
+        .attr('y', 0)
+        .attr('width', 50)
+        .attr('height', d => d.height)
+        .attr('fill', 'grey');
 
-    const colour = (d) => d.name === 'Jessica' ? 'blue' : 'red';
-
-    circles.enter().append('circle')
-        .attr('cx', (d, i) => (i * 50) + 50)
-        .attr('cy', 250)
-        .attr('r', (d) => 2 * d.age)
-        .attr('fill', colour);
 }).catch(error => {
     console.log(error);
 });
