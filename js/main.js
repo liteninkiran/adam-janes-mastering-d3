cda5c3a7461b9ca215bfd0839606ea4b22585afc
Kiran Anand <liteninkiran@hotmail.com>
2024-03-23 16:33:11 +0000

    18 - Linear Scales

--- js/main.js
+++ js/main.js
@@ -4,17 +4,22 @@ d3.json('./data/buildings.json').then(data => {
     const svg = d3.select('#chart-area')
         .append('svg')
         .attr('style', border)
-        .attr('width', 500)
-        .attr('height', 500);
+        .attr('width', 400)
+        .attr('height', 400);
 
+    const buffer = 50;
+    const maxValue = 828;
     const rects = svg.selectAll('rect').data(data);
+    const y = d3.scaleLinear()
+        .domain([0, maxValue + buffer])
+        .range([0, 400]);
 
     rects.enter()
         .append('rect')
-        .attr('x', (d, i) => (i * 80) + 50)
+        .attr('x', (d, i) => (i * 60) + 20)
         .attr('y', 0)
-        .attr('width', 50)
-        .attr('height', d => d.height)
+        .attr('width', 40)
+        .attr('height', d => y(d.height))
         .attr('fill', 'grey');
 
 }).catch(error => {
