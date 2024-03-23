0359e00344cfbf7f30a41109c307840a17cf4f94
Kiran Anand <liteninkiran@hotmail.com>
2024-03-23 17:17:14 +0000

    26 - Making a bar chart

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ d3.json('data/buildings.json').then(data => {
     // Define Y-Axis
     const y = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.height)])
-        .range([0, HEIGHT]);
+        .range([HEIGHT, 0]);
 
     const xAxisCall = d3.axisBottom(x);
     g.append('g')
@@ -76,9 +76,9 @@ d3.json('data/buildings.json').then(data => {
 
     // Add bars
     rects.enter().append('rect')
-        .attr('y', 0)
+        .attr('y', d => y(d.height))
         .attr('x', (d) => x(d.name))
         .attr('width', x.bandwidth)
-        .attr('height', d => y(d.height))
+        .attr('height', d => HEIGHT - y(d.height))
         .attr('fill', 'grey');
 });
