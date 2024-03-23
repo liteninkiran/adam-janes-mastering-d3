306f67633a9a0804e7d3967a272cf8c235350a40
Kiran Anand <liteninkiran@hotmail.com>
2024-03-23 16:46:16 +0000

    22 - Band Scales

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
-d3.json('./data/buildings.json').then(data => {
-    data.forEach(d => d.height = Number(d.height));
-    const border = 'border: 1px solid lightgrey; border-radius: 20px;';
-    const svg = d3.select('#chart-area')
-        .append('svg')
-        .attr('style', border)
-        .attr('width', 400)
-        .attr('height', 400);
+const border = 'border: 1px solid lightgrey; border-radius: 5px;';
+const svg = d3.select('#chart-area')
+    .append('svg')
+    .attr('style', border)
+    .attr('width', 400)
+    .attr('height', 400);
 
-    const buffer = 50;
-    const maxValue = 828;
-    const rects = svg.selectAll('rect').data(data);
+d3.json('data/buildings.json').then(data => {
+    data.forEach(d => d.height = Number(d.height));
+    const names = data.map(building => building.name);
+    const x = d3.scaleBand()
+        .domain(names)
+        .range([0, 400])
+        .paddingInner(0.3)
+        .paddingOuter(0.2)
+    console.log(x(names[1]));
     const y = d3.scaleLinear()
-        .domain([0, maxValue + buffer])
+        .domain([0, 828 + 20])
         .range([0, 400]);
 
-    rects.enter()
-        .append('rect')
-        .attr('x', (d, i) => (i * 60) + 20)
+    const rects = svg
+        .selectAll('rect')
+        .data(data);
+
+    rects.enter().append('rect')
         .attr('y', 0)
-        .attr('width', 40)
+        .attr('x', (d) => x(d.name))
+        .attr('width', x.bandwidth)
         .attr('height', d => y(d.height))
         .attr('fill', 'grey');
-
-}).catch(error => {
-    console.log(error);
 });
