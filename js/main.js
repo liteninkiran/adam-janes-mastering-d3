34ca8483e4f327eceb9c1eac0aa9fa4dc829a5d7
Kiran Anand <liteninkiran@hotmail.com>
2024-03-23 14:18:14 +0000

    13 - Selections and data joins

--- js/main.js
+++ js/main.js
@@ -1,38 +1,13 @@
-const border = 'border: 1px solid lightgrey; border-radius: 20px;';
-const svg = d3.select('#chart-area')
-    .append('svg')
-    .attr('style', border)
-    .attr('width', 500)
-    .attr('height', 400);
+const data = [2, 4, 6, 8, 10];
 
-svg.append('circle')
-    .attr('cx', 100)
-    .attr('cy', 100)
-    .attr('r', 70)
-    .attr('fill', 'red');
+const svg = d3.select('#chart-area').append('svg')
+	.attr('width', 400)
+	.attr('height', 400)
 
-const y = 100;
-const x = 300;
-const length = 100;
+const circles = svg.selectAll('circle').data(data);
 
-svg.append('line')
-    .attr('x1', x)
-    .attr('y1', y)
-    .attr('x2', x + length)
-    .attr('y2', y)
-    .attr('stroke', 'blue')
-    .attr('stroke-width', 5);
-
-svg.append('rect')
-    .attr('x', 50)
-    .attr('y', 230)
-    .attr('width', 100)
-    .attr('height', 100)
-    .attr('fill', 'green');
-
-svg.append('ellipse')
-    .attr('cx', 350)
-    .attr('cy', 280)
-    .attr('rx', 100)
-    .attr('ry', 50)
-    .attr('fill', 'red');
+circles.enter().append('circle')
+	.attr('cx', (d, i) => (i * 50) + 50)
+	.attr('cy', 250)
+	.attr('r', (d) => d)
+	.attr('fill', 'red');
