82addc1d5ce785c429388c90e1873c241a0acc95
Kiran Anand <liteninkiran@hotmail.com>
2024-03-23 14:45:48 +0000

    14 - Loading external data

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-const data = [2, 4, 6, 8, 10];
+d3.json('./data/ages.json').then(data => {
+    console.log(data);
+    data.forEach(d => d.age = Number(d.age));
 
-const svg = d3.select('#chart-area').append('svg')
-	.attr('width', 400)
-	.attr('height', 400)
+    const svg = d3.select('#chart-area').append('svg')
+        .attr('width', 400)
+        .attr('height', 400);
 
-const circles = svg.selectAll('circle').data(data);
+    const circles = svg.selectAll('circle')
+        .data(data);
 
-circles.enter().append('circle')
-	.attr('cx', (d, i) => (i * 50) + 50)
-	.attr('cy', 250)
-	.attr('r', (d) => d)
-	.attr('fill', 'red');
+    const colour = (d) => d.name === 'Jessica' ? 'blue' : 'red';
+
+    circles.enter().append('circle')
+        .attr('cx', (d, i) => (i * 50) + 50)
+        .attr('cy', 250)
+        .attr('r', (d) => 2 * d.age)
+        .attr('fill', colour);
+}).catch(error => {
+    console.log(error);
+});
