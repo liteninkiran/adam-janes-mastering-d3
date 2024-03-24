540daa866d6bc745401ab5b3e2d31bed570c068c
Kiran Anand <liteninkiran@hotmail.com>
2024-03-24 18:15:30 +0000

    47 - Activity: Project 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,117 @@
-d3.json('data/data.json').then(function(data) {
-	console.log(data);
-});
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT;
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM;
+
+const svg = d3.select('#chart-area')
+	.append('svg')
+	.attr('width', WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
+	.attr('height', HEIGHT + MARGIN.TOP + MARGIN.BOTTOM);
+
+const transform = `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`;
+
+const g = svg.append('g')
+	.attr('transform', transform);
+
+let time = 0;
+
+// Scales
+const x = d3.scaleLog()
+	.base(10)
+	.range([0, WIDTH])
+	.domain([142, 150000]);
+
+const y = d3.scaleLinear()
+	.range([HEIGHT, 0])
+	.domain([0, 90]);
+
+const area = d3.scaleLinear()
+	.range([25 * Math.PI, 1500 * Math.PI])
+	.domain([2000, 1400000000]);
+
+const continentColour = d3.scaleOrdinal(d3.schemePastel1);
+
+// Labels
+const xLabel = g.append('text')
+	.attr('y', HEIGHT + 50)
+	.attr('x', WIDTH / 2)
+	.attr('font-size', '20px')
+	.attr('text-anchor', 'middle')
+	.text('GDP Per Capita ($)');
+const yLabel = g.append('text')
+	.attr('transform', 'rotate(-90)')
+	.attr('y', -40)
+	.attr('x', -170)
+	.attr('font-size', '20px')
+	.attr('text-anchor', 'middle')
+	.text('Life Expectancy (Years)');
+const timeLabel = g.append('text')
+	.attr('y', HEIGHT - 10)
+	.attr('x', WIDTH - 40)
+	.attr('font-size', '40px')
+	.attr('opacity', '0.4')
+	.attr('text-anchor', 'middle')
+	.text('1800');
+
+// X Axis
+const xAxisCall = d3.axisBottom(x)
+	.tickValues([400, 4000, 40000])
+	.tickFormat(d3.format('$'));
+g.append('g')
+	.attr('class', 'x axis')
+	.attr('transform', `translate(0, ${HEIGHT})`)
+	.call(xAxisCall);
+
+// Y Axis
+const yAxisCall = d3.axisLeft(y)
+g.append('g')
+	.attr('class', 'y axis')
+	.call(yAxisCall);
+
+d3.json('data/data.json').then(function (data) {
+	// Clean data
+	const formattedData = data.map(year => {
+		return year['countries'].filter(country => {
+			const dataExists = (country.income && country.life_exp);
+			return dataExists;
+		}).map(country => {
+			country.income = Number(country.income);
+			country.life_exp = Number(country.life_exp);
+			return country;
+		});
+	});
+
+	// Run the code every 0.1 second
+	d3.interval(function () {
+		// At the end of our data, loop back
+		time = (time < 214) ? time + 1 : 0;
+		update(formattedData[time]);
+	}, 100);
+
+	// First run of the visualization
+	update(formattedData[0]);
+})
+
+function update(data) {
+	// Standard transition time for the visualization
+	const t = d3.transition()
+		.duration(100);
+
+	// JOIN new data with old elements
+	const circles = g.selectAll('circle')
+		.data(data, d => d.country);
+
+	// EXIT old elements not present in new data
+	circles.exit().remove();
+
+	// ENTER new elements present in new data
+	circles.enter().append('circle')
+		.attr('fill', d => continentColour(d.continent))
+		.merge(circles)
+		.transition(t)
+		.attr('cy', d => y(d.life_exp))
+		.attr('cx', d => x(d.income))
+		.attr('r', d => Math.sqrt(area(d.population) / Math.PI));
+
+	// Update the time label
+	timeLabel.text(String(time + 1800));
+}
